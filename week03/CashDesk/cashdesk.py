1dee07a2b67988606c88df66a5ea145155d933c7
Yoanna <yoansiyotova@gmail.com>
2020-03-21 12:55:11 +0200

    Add solution to CashDesk

---
+++ week03/CashDesk/cashdesk.py
@@ -0,0 +1,112 @@
+from operator import attrgetter
+
+class Bill:
+    money_holder = {}
+
+    def __init__(self, amount):
+        if amount < 0:
+            raise ValueError('Amount cannot be negative')
+        elif type(amount) != int:
+            raise TypeError('Type of amount should be int')
+        else:
+            self.amount = amount
+
+            if str(self) in self.money_holder:
+                self.money_holder[str(self)] += 1
+            else:
+                self.money_holder[str(self)] = 1
+
+
+    def __str__(self):
+        return f'A {self.amount}$ bill'
+
+    def __repr__(self):
+        return f'A {self.amount}$ bill'
+
+    def __int__(self):
+        return self.amount
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def __hash__(self):
+        return self.money_holder[str(self)]
+
+class BatchBill:
+    
+    def __init__(self, bills):
+        self.bills = bills
+
+    def __getitem__(self, index):
+        return self.bills[index]
+
+    def __len__(self):
+        count_of_bills = 0
+        for bill in self.bills:
+            count_of_bills += 1
+
+        return count_of_bills
+
+    def total(self):
+        count_of_amount = 0
+        for bill in self.bills:
+            count_of_amount += bill.amount
+
+        return count_of_amount
+
+class CashDesk:
+
+    def __init__ (self):
+        self.money_of_bills = []
+
+    def take_money(self, money):
+        self.money = money
+        
+        #all the bills append to the list of the class
+        if type(self.money) is Bill:
+            self.money_of_bills.append(self.money)
+        else:
+            for i in range(len(money)):
+                self.money_of_bills.append(self.money[i])
+
+    def total(self):
+        count = 0
+        for bill in self.money_of_bills:
+            count += int(bill)
+        return count
+
+    def inspect(self):
+        # sort by amount 
+        self.money_of_bills = sorted(self.money_of_bills, key = attrgetter('amount'))
+
+        print(f'We have a total of {self.total()}$ in the desk')
+        print("We have the following count of bills, sorted in ascending order:")
+
+        for i in range(len(self.money_of_bills) - 1):
+            #check for repeating bills
+            if self.money_of_bills[i] != self.money_of_bills[i + 1]:
+                print(f'{self.money_of_bills[i].amount}$ bills - {Bill.money_holder[str(self.money_of_bills[i])]}')
+
+        #print the last one
+        last_bill_index = len(self.money_of_bills) - 1
+        print(f'{self.money_of_bills[last_bill_index].amount}$ bills - {Bill.money_holder[str(self.money_of_bills[last_bill_index])]}')
+  
+def main():
+    values = [10, 20, 50, 100, 100, 100]
+    bills = [Bill(value) for value in values]
+
+    batch = BatchBill(bills)
+
+    desk = CashDesk()
+
+    desk.take_money(batch)
+    desk.take_money(Bill(10))
+
+    print(desk.total()) 
+    desk.inspect()
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ week03/CashDesk/test_cashdesk.py
@@ -0,0 +1,145 @@
+import unittest
+from cashdesk import Bill, BatchBill, CashDesk
+
+class TestBill(unittest.TestCase):
+    def test_with_valid_amount(self):
+        Bill.money_holder = {}
+        a = Bill(10)
+        
+        self.assertEqual(a.amount, 10)
+
+    def test_the_amount_is_negative_number_and_rase_value_error(self):
+        exc = None
+
+        try:
+            Bill(-10)
+        except ValueError as err:
+            exc = err
+
+        self.assertIsNotNone(str(exc))
+        self.assertEqual(str(exc), 'Amount cannot be negative')
+
+    def test_type_the_amount_is_not_int_and_raise_type_error(self):
+        exc = None
+
+        try:
+            Bill(2.5)
+        except TypeError as err:
+            exc = err
+
+        self.assertIsNotNone(str(exc))
+        self.assertEqual(str(exc),'Type of amount should be int')
+
+    def test_representions_as_string(self):
+        Bill.money_holder = {}
+        a = Bill(10)
+        
+        self.assertEqual(str(a), "A 10$ bill")
+
+    def test_converting_the_object_into_the_amount_with_int(self):
+        Bill.money_holder = {} 
+        a = Bill(10)
+        
+        self.assertEqual(int(a), 10)
+
+    def test_two_bills_are_equel_if_they_have_same_amount(self):
+        Bill.money_holder = {}
+        a = Bill(100)
+        b = Bill(100)
+        
+        self.assertTrue(a == b, 'Bills are not equal')
+
+    def test_hashing_how_many_bills_are_in_the_money_holder(self):
+        Bill.money_holder = {}
+        a = Bill(20)
+        c = Bill(20)
+        
+        self.assertEqual(Bill.money_holder[str(a)], 2)
+        
+class TestBatchBill(unittest.TestCase):
+    def test_getting_item_from_list_of_bills(self):
+        Bill.money_holder = {}
+        a = Bill(10)
+        b = Bill(20)
+
+        batch = BatchBill([a,b])
+
+        self.assertEqual(batch[0], a)
+
+    def test_counting_the_numbers_of_bills_in_the_batch(self):
+        Bill.money_holder = {}
+        a = Bill(10)
+        b = Bill(20)
+
+        batch = BatchBill([a,b])
+
+        self.assertEqual(len(batch), 2)
+
+    def test_total_amount_of_all_bills_in_the_batch(self):
+        Bill.money_holder = {}
+        a = Bill(10)
+        b = Bill(20)
+
+        batch = BatchBill([a,b])
+
+        self.assertEqual(batch.total(), 30)
+
+
+class TestCashDesk(unittest.TestCase):
+
+    def test_total_amount_of_money_in_the_desk_with_bills(self):
+        Bill.money_holder = {}
+
+        a = Bill(10)
+        b = Bill(20)
+        c = Bill(30)
+        
+        desk = CashDesk()
+
+        desk.take_money(a)
+        desk.take_money(b)
+        desk.take_money(c)
+
+        total = desk.total()
+
+        self.assertEqual(total, 60)
+
+    def test_total_amount_of_money_in_the_desk_with_batches(self):
+        Bill.money_holder = {}
+
+        a = Bill(10)
+        b = Bill(20)
+        c = Bill(40)
+
+        batch = BatchBill([a,b,c])
+
+        desk1 = CashDesk()
+
+        desk1.take_money(batch)        
+
+        total= desk1.total()
+
+        self.assertEqual(total, 70)
+
+    def test_total_amount_of_money_in_the_desk_with_bills_and_batches(self):
+        Bill.money_holder = {}
+
+        a = Bill(10)
+        b = Bill(20)
+        c = Bill(40)
+
+        batch = BatchBill([a,b,c])
+        desk = CashDesk()
+
+        desk.take_money(batch)
+        desk.take_money(a)
+
+        total= desk.total()
+
+        self.assertEqual(total, 80)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
